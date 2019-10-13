9127bf754a9bad8dad18a9ed96f38450db9051a7
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-13 11:57:16 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,25 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "Write your SQL query Here"
+  "SELECT title, sum(amount) from projects left join pledges on projects.id = pledges.project_id group by title order by title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Write your SQL query Here"
+  "SELECT NAME, AGE, SUM(AMOUNT) FROM PLEDGES LEFT JOIN USERS ON PLEDGES.USER_ID = USERS.ID GROUP BY NAME, AGE ORDER BY NAME;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT TITLE, SUM(AMOUNT) - FUNDING_GOAL FROM PROJECTS LEFT JOIN PLEDGES ON PROJECTS.ID = PLEDGES.PROJECT_ID GROUP BY TITLE HAVING SUM(AMOUNT) >= FUNDING_GOAL;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT NAME, SUM(AMOUNT) FROM USERS LEFT JOIN PLEDGES ON USERS.ID = PLEDGES.USER_ID GROUP BY NAME ORDER BY SUM(AMOUNT); "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT CATEGORY, AMOUNT FROM PROJECTS LEFT JOIN PLEDGES ON PROJECTS.ID = PLEDGES.PROJECT_ID WHERE CATEGORY = 'music'; "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT CATEGORY, SUM(AMOUNT) FROM PROJECTS LEFT JOIN PLEDGES ON PROJECTS.ID = PLEDGES.PROJECT_ID WHERE CATEGORY = 'books' GROUP BY CATEGORY;"
 end
